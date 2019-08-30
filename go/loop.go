4b9f55549966fcac6e7e9de881bfba053a65f4da
luffyme <893954253@qq.com>
2019-08-30 16:11:05 +0000

    update go learning

--- go/loop.go
+++ go/loop.go
@@ -5,19 +5,19 @@ import (
 )
 
 func main() {
-	//3.for 循环
+	//1.for 循环
 	//Go 语言虽然没有提供 while 和 do while 语句，不过这两个语句都可以使用 for 循环的形式来模拟。
 	//平时使用 while 语句来写死循环 while(true) {}，Go 语言可以这么写
 	/* 
-	for {
-        fmt.Println("hello world!")
-	}
+		for {
+			fmt.Println("hello world!")
+		}
 	*/
 	//也可以这样写，效果是一样的
 	/* 
-	for true {
-        fmt.Println("hello world!")
-	}
+		for true {
+			fmt.Println("hello world!")
+		}
 	*/
 	//for 什么条件也不带的，相当于 loop {} 语句。
 	//for 带一个条件的相当于 while 语句
@@ -26,7 +26,7 @@ func main() {
 		fmt.Println("hello world!")
 	}
 	
-	//4.循环控制
+	//2.循环控制
 	//Go 语言支持 continue 和 break 语句来控制循环，这两个语句和其它语言没什么特殊的区别。
 	//除此之外 Go 语言还支持 goto 语句，我们作为普通用户平时还是尽量不要使用 goto 语句了。
 }
