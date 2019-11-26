95b4951435374053a3db5f4831c2cb525c8ae295
luffyme <893954253@qq.com>
2019-11-26 09:03:03 +0800

    update

--- project/gin_blog/go.mod
+++ project/gin_blog/go.mod
@@ -6,9 +6,17 @@ require (
 	github.com/astaxie/beego v1.12.0
 	github.com/davecgh/go-spew v1.1.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/gin-gonic/gin v1.4.0
+	github.com/gin-contrib/cors v1.3.0 // indirect
+	github.com/gin-contrib/pprof v1.2.1
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/jinzhu/gorm v1.9.10
+	github.com/json-iterator/go v1.1.8 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/unknwon/com v1.0.1 // indirect
-	gopkg.in/yaml.v2 v2.2.2
+	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
+	gopkg.in/go-playground/validator.v9 v9.30.0 // indirect
+	gopkg.in/yaml.v2 v2.2.7
 )
--- project/gin_blog/router/router.go
+++ project/gin_blog/router/router.go
@@ -5,9 +5,35 @@ import (
 	"gin_blog/middleware"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/pprof"
+	"github.com/gin-contrib/cors"
 )
 
 func Add(r *gin.Engine) *gin.Engine {
+	pprof.Register(r, "debug/pprof")
+
+	/*
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://google.com"}
+	r.Use(cors.New(config))
+	*/
+
+	//跨域
+	r.Use(cors.Default())
+
+
 	r.GET("/ping", service.Ping)
 	r.GET("/auth", service.GetAuth)
 	r.GET("/upload", service.UploadImage)
