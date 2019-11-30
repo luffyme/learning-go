daf19a2f16d700a8884e9c41a82979d89fb370be
luffyme <893954253@qq.com>
2019-11-30 08:42:36 +0800

    json

--- go_standard_library/encoding/json/json.Marshal.go
+++ go_standard_library/encoding/json/json.Marshal.go
@@ -12,16 +12,39 @@ type ColorGroup struct {
 }
 
 func main() {
+
+
+	//bool
+	bolB, _ := json.Marshal(true)
+	fmt.Println(string(bolB))
+	
+	//int
+    intB, _ := json.Marshal(1)
+	fmt.Println(string(intB))
+	
+	//float
+    fltB, _ := json.Marshal(2.34)
+	fmt.Println(string(fltB))
+	
+	//string
+    strB, _ := json.Marshal("gopher")
+    fmt.Println(string(strB))
+
+	//slice
+    slcD := []string{"apple", "peach", "pear"}
+    slcB, _ := json.Marshal(slcD)
+	fmt.Println(string(slcB))
+	
+	//map
+    mapD := map[string]int{"apple": 5, "lettuce": 7}
+    mapB, _ := json.Marshal(mapD)
+    fmt.Println(string(mapB))
+
 	group := ColorGroup{
 		ID: 1,
 		Name: "redis",
 		Colors: []string{"crimson", "red", "ruby", "maroon"},
 	}
-
-	j, err := json.Marshal(group)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-
+	j, _ := json.Marshal(group)
 	fmt.Println(string(j))
 }
